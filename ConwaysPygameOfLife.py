6b38f6e4c799c27cf96909ee931493ebcd57fc07
Robert De La Cruz II <robdlcii@gmail.com>
2019-06-19 20:45:01 -0400

    Added commented logic
    Typed in some basic logic for the game (simulation), proper code and to come.

--- ConwaysPygameOfLife.py
+++ ConwaysPygameOfLife.py
@@ -42,6 +42,56 @@ while not done:
  
     # --- Game logic should go here
 
+    """
+    # Written by Robert De La Cruz II, based on Conway's Game of Life
+    # Rules found at https://study.com/academy/lesson/conways-game-of-life-rules-instructions.html
+    # 1. Birth: Any dead cell with 3 live neighbors will come to life in the next generation
+             if (adjacent && alive == 3) { currentCell.activate() }
+    
+    # 2. Death by Isolation: Any live cell with 1 or fewer neighbors will die in the next generation
+            else if (adjacent && alive < 2) { currentCell.deactivate() }
+
+    # 3. Death by Overcrowding: Any live cell with 4 or more neighbors will die in the next generation
+            else if (adjacent && alive > 3) { currentCell.deactivate() }
+
+    # 4. Survival: Any cell with 2 or 3 living neighbors will remain alive in the next generation
+            else { "Do nothing" }
+
+    # The grid for this will wrap around
+
+    # Variables
+    gridSize = 300
+    mainGrid = [gridSize,gridSize]
+    storageGrid = [gridSize.gridSize] # As the name implies, this is for storing the main grid during calculations
+
+    def nextGen():
+        storageGrid = mainGrid
+
+        for each cell in mainGrid_X-Axis 
+            for each cell in mainGrid_Y-Axis
+                if (adjacent && alive == 3)
+                    currentCell.activate()
+                else if (adjacent && alive < 2) 
+                    currentCell.deactivate()
+                else if (adjacent && alive > 3)
+                    currentCell.deactivate()
+                else
+                    "Do nothing"
+            
+    # display( mainGrid )
+
+    # Pause function
+    def (onclick)
+        while(true || oneGen)
+            # Forward 1 generation
+            oneGen = true
+            # Forward
+            break
+
+    
+    Repeat until program is closed
+    """
+
     # --- Screen-clearing code goes here
  
     # Here, we clear the screen to white. Don't put other drawing commands
@@ -52,6 +102,7 @@ while not done:
     screen.fill(WHITE)
  
     # --- Drawing code should go here
+    """ display (mainGrid) """
  
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
