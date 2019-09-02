19c84debdcae1789023ac784a51bed4a22b96aee
Robert De La Cruz II <robdlcii@gmail.com>
2019-09-02 02:09:02 -0400

    With loads of help, many issues solved
    Still some quirks, may need to fix things

--- ConwaysPygameOfLife.py
+++ ConwaysPygameOfLife.py
@@ -5,15 +5,12 @@
     2. Death by Isolation: Any live cell with 1 or fewer neighbors will die in the next generation
     3. Death by Overciding: Any live cell with 4 or more neighbors will die in the next generation
     4. Survival: Any cell with 2 or 3 living neighbors will remain alive in the next generation
-
  The grid for this will wrap around
-
  Code Sources:
  https://www.geeksforgeeks.org/conways-game-life-python-implementation/
  http://programarcadegames.com/python_examples/f.php?file=array_backed_grid.py
  https://docs.python.org/3/library/random.html
  https://stackoverflow.com/questions/15884527/how-can-i-prevent-the-typeerror-list-indices-must-be-integers-not-tuple-when-c
-
  Pygame base template for opening a window
  
  Sample Python/Pygame Programs
@@ -29,9 +26,9 @@ import random
  
 
 # Combination or ease variables.
-tickTock = 60                         # Clock speed AKA Number of loops per second
-gridSize = 200                       # Bringing all the grid related variables into a unified variable, the rest is simple math.
-seedOfLife = 15                     # Sets the upper bound of the random.randint that is called to decide which cells are seeded. The chance of life is 1/seedOfLife
+tickTock = 60               # Clock speed AKA Number of loops per second
+gridSize = 200              # Bringing all the grid related variables into a unified variable, the rest is simple math.
+seedOfLife = 15             # Sets the upper bound of the random.randint that is called to decide which cells are seeded. The chance of life is 1/seedOfLife
 BACKGROUND = (0, 0, 0)      # For setting a background color without a visible grid in a single variable
 
 # Define some colors
@@ -61,16 +58,18 @@ for i in range(gridSize):
     for j in range(gridSize):
         grid[i].append(0)  # Append a cell
         nextGrid[i].append(0)
+        if (random.randint(1,seedOfLife) is int(seedOfLife/2)):
+            grid[ i ][ j ] = ON
 
 
 # Seed the grid with life
-for i in range(gridSize):
-    for j in range(gridSize):
-            if  (random.randint(1,seedOfLife) is int(seedOfLife/2)):
-                nextGrid[ i ][ j ] = ON
+#for i in range(gridSize):
+#    for j in range(gridSize):
+#        if  (random.randint(1,seedOfLife) is int(seedOfLife/2)):
+#            grid[ i ][ j ] = ON
 
 # Seed the nextGrid, currently both of them are identical
-grid = nextGrid
+#grid = nextGrid
 
 # The game code (runtime logic) starts here
 pygame.init()
@@ -90,41 +89,55 @@ clock = pygame.time.Clock()
 
 # -------- Main Program Loop -----------
 while not done:
-    for event in pygame.event.get():    # User did something
+    for event in pygame.event.get():   # User did something
         if event.type == pygame.QUIT:  # If user clicked close
-            done = True                            # Flag that we are done so we exit this loop
+            done = True                # Flag that we are done so we exit this loop
  
     # Set the screen background color (Grid lines)
     screen.fill(BACKGROUND) 
- 
-    # Copy grid into nextGrid
-    nextGrid = list(grid)
 
     # Comparisons are made on grid. Changes made to nextGrid.
     for i in range(gridSize):
         for j in range(gridSize):
-            color = BACKGROUND
-            total = ( (     grid[ i ] [ ( j - 1 ) % gridSize ] +                                               # To the Top
-                                grid[ i ] [ (  j + 1 ) % gridSize ] +                                             # To the Bottom
-                                grid[ ( i - 1 ) % gridSize] [ j ] +                                                # To the Left 
-                                grid[ ( i + 1 ) % gridSize] [ j ] +                                               # To the Right
-                                grid[ ( i - 1 ) % gridSize] [ ( j - 1 ) % gridSize ] +                     # Below and to the Left
-                                grid[ ( i - 1 ) % gridSize] [ ( j +1 ) % gridSize ] +                     # Above and to the Left
-                                grid[ ( i + 1 ) % gridSize] [ ( j - 1 ) % gridSize ] +                    # Below and to the Right
-                                grid[ ( i + 1 ) % gridSize] [ ( j +1 ) % gridSize ] ) )                   # Above and to the Right
+            total = ( (     grid[ i ] [ ( j - 1 ) % gridSize ] +                                      # To the Top
+                            grid[ i ] [ ( j + 1 ) % gridSize ] +                                      # To the Bottom
+                            grid[ ( i - 1 ) % gridSize] [ j ] +                                       # To the Left 
+                            grid[ ( i + 1 ) % gridSize] [ j ] +                                       # To the Right
+                            grid[ ( i - 1 ) % gridSize] [ ( j - 1 ) % gridSize ] +                    # Below and to the Left
+                            grid[ ( i - 1 ) % gridSize] [ ( j +1 ) % gridSize ] +                     # Above and to the Left
+                            grid[ ( i + 1 ) % gridSize] [ ( j - 1 ) % gridSize ] +                    # Below and to the Right
+                            grid[ ( i + 1 ) % gridSize] [ ( j +1 ) % gridSize ] ) )                   # Above and to the Right
 
             # Conway's rules
-            if (grid[ i ][ j ] == ON):                      # If alive
-                if (total < 2) or (total > 3):            # And it is Isolated or Overcrowded
-                    nextGrid[ i ][ j ] = OFF               # It dies
+            if (grid[ i ][ j ] == ON):                  # If alive
+                if (total < 2) or (total > 3):          # If starved or overcrowded
+                    nextGrid[ i ][ j ] = OFF            # It dies
                     color = BACKGROUND
-            else:                                                   # If dead
-                    if total == 3:                              # And the number of living adjacent cells is 3
-                        nextGrid[ i ][ j ] = ON            # It comes to life
-                        color = GREEN
+            else:                                       # If off
+                if total == 3:
+                    nextGrid[ i ][ j ] = ON             # It comes to life
+                    color = GREEN
+                else:
+                    nextGrid[ i ][ j ] = OFF            # It stays dead
+                    color = BACKGROUND
+
+            #color = BACKGROUND
+            #nextGrid[i][j] = OFF
+            #if (total == 2 and grid[i][j] == ON) or total == 3:
+            #     nextGrid[i][j] = ON
+            #     color = GREEN
+
+            #if (grid[ i ][ j ] == ON):                    # If alive
+            #    if (total < 2) or (total > 3):            # And it is Isolated or Overcrowded
+            #        nextGrid[ i ][ j ] = OFF              # It dies
+            #        color = BACKGROUND
+            #else:                                         # If dead
+            #        if total == 3:                        # And the number of living adjacent cells is 3
+            #            nextGrid[ i ][ j ] = ON           # It comes to life
+            #            color = GREEN
 
             # Copy nextGrid, with its updated info, onto grid.
-            grid = list(nextGrid)
+            # grid = list(nextGrid)
             
             # This redraws the grid every time
             pygame.draw.rect( screen,
@@ -133,7 +146,14 @@ while not done:
                              MARGIN, ( MARGIN + HEIGHT ) * i + 
                               MARGIN, WIDTH, HEIGHT ] )
 
-     # --- Limit frames per second (Number of next generations per second)
+    swapv = grid
+    grid = nextGrid
+    nextGrid = swapv
+    #for i in range(gridSize):
+    #    for j in range(gridSize):
+    #        grid[i][j] = nextGrid[i][j]
+    
+    # --- Limit frames per second (Number of next generations per second)
     clock.tick(tickTock)
 
     # --- Go ahead and update the screen with what we've drawn.
@@ -141,4 +161,15 @@ while not done:
 
 # Be IDLE friendly. If you forget this line, the program will 'hang' on exit.
 pygame.quit()
+ 
+
+ 
+ # Start with Grid, do my thing.  Change nextgrid as I go
+ # Grid needs to be reset to what nextgrid is
+ # swapv=grid
+ # nextgrid=swapv
+
+ # look into copying entire 2d arrays instead of iterative copy
+
+ # if (total == 2 and g[i][j] == ON) or total == 3: ng[i][j] = ON
  
